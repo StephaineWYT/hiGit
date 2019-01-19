28bd192ed1073446e5bac153176a789af81feefe
StephanieWYT <820352756@qq.com>
2019-01-19 10:43:24 +0800

    test

--- src/HiGit.java
+++ src/HiGit.java
@@ -1,5 +1,8 @@
+import java.util.Date;
+
 public class HiGit {
     public static void main(String[] args){
-        System.out.println("hi Git!");
+        System.out.println("present is present!");
+        System.out.println(new Date().toString());
     }
 }
